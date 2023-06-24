16a97428d0b72b984f04033e3a42fb704464014a
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-06-24 04:29:08 -0400

    Added choice element in main.cpp

--- 1-C++ Basics/main.cpp
+++ 1-C++ Basics/main.cpp
@@ -3,14 +3,23 @@
 #include "SumNN.h"
 
 int main() {
-    std::cout << "------ Calling the area() function ------" << std::endl;
-    area();
+    int choice;
+    std::cout <<"Please enter the program you want to run (1 for triangle area, 2 for sum of natural numbers):";
+    std:: cin>>choice;
 
-    std::cout << "\n----- Calling the sumOfN() function -----" << std::endl;
-    int n;
-    std::cout << "Enter a natural number: ";
-    std::cin >> n;
-    sumOfN(n);
+    if (choice ==1) {
+        std::cout << "------ Calling the area() function ------" << std::endl;
+        area();
+    }
+
+    else if (choice ==2) {
+
+        std::cout << "\n----- Calling the sumOfN() function -----" << std::endl;
+        int n;
+        std::cout << "Enter a natural number: ";
+        std::cin >> n;
+        sumOfN(n);
+    }
 
     return 0;
 }
